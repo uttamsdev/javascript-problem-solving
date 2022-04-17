d183277c8c8d34c9093a5c95c24a4956eb50cff5
UttamSaha <uttamsaha982@gmail.com>
2022-04-17 23:11:05 +0600

    updated

--- Basic Problems/4-odd-length-friend.js
+++ Basic Problems/4-odd-length-friend.js
@@ -3,13 +3,14 @@ function oddFriend(friends) {
   if (Array.isArray(friends) != true) {
     return "Your input must be an array.";
   }
+
   for (const friend of friends) {
     length = friend.length;
     if (length % 2 != 0) {
       return friend;
     }
   }
-  return "There is no odd length of your friends name";
+  return "There is no odd length of your friends name..";
 }
 var myFriends = ["Luna", "Sahkib", "Akib", "Jolil", "Komol", "Oggy"];
 var friend = oddFriend(myFriends);
